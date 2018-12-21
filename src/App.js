cfa25a0c8109db90c5e4dac08bbd154762b606ed
Dmitriy <11lione1991@gmail.com>
2018-12-22 00:46:37 +0200

    delete combineReducer, will add equal prevState and currentState

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import './App.css';
 
 
 class App extends Component {
+	state = {
+		popMoviesList: []
+	}
+
 	componentDidMount() {
-		this.props.getPopularMovie()
-  }
+		const pop = this.props.getPopularMovie()
+		// console.log('pop in did mount', pop)
+		// this.setState({popMoviesList})
+		// console.log(this.props.popularMovieList)
+		console.log('props in componentDidMount', this.props)
+	}
+
+	componentDidUpdate() {
+		// this.setState({popMoviesList: this.props.popularMovieList})
+		console.log('props in componentDidupdate', this.props)
+	}
+	
 	// render list movie when request will be success
   render() {
-
     return (
       <div className="App">
 				<header>here header</header>
+				{/* 
+					{props.popularMoviesList.map( movie => <Showmovie key={movie.id} movie={movie} ... )}
+				*/}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,24 +6,27 @@ import * as serviceWorker from './serviceWorker';
 //https://codepen.io/BenSmith/pen/boapwY?editors=0010//
 import { combineReducers, applyMiddleware, createStore, compose } from 'redux'
 import { Provider, connect } from 'react-redux'
-import createSagaMiddleware, { takeEvery } from 'redux-saga'
-import { call, put } from 'redux-saga/effects'
+import createSagaMiddleware from 'redux-saga'
+import { call, put, takeEvery } from 'redux-saga/effects'
 import axios from 'axios'
 
 // API request
-const requestPopularMovie = () => axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=e842780f24447ce021759d2711fd23ce&language=en-US&page=1')
-	.then( response => response )
+const requestPopularMovie = () => 
+  axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=e842780f24447ce021759d2711fd23ce&language=en-US&page=1')
+	
 
 // write action
 const getPopularMovie = () => ({ 
 	type: 'GET_LOAD_MOVIE'
 })
 
+
 // SAGA
 function* getPopularMovieSaga() {
 	try {
 		const result = yield call( requestPopularMovie )
-		yield put({type: 'GET_POPULAR_MOVIE', popularMovieList: result.data.results } )
+		console.log( result )
+		yield put({type: 'GET_POPULAR_MOVIE', popularMovieList: result.data.results })
 	}	catch(error) {
 		console.log(error.message)
 	}
@@ -33,8 +36,14 @@ function* rootSaga() {
 	yield takeEvery('GET_LOAD_MOVIE', getPopularMovieSaga)
 }
 
+// initialState
+const initialState = {
+	movie: null,
+	popularMovieList: null
+}
+
 // create reducer with combineReducers
-const movieReducer = ( state = {}, action ) => {
+const movieReducer = ( state = initialState, action ) => {
 	switch( action.type ) {
 		case 'GET_LOAD_MOVIE':
 		console.log('action in GET_LOAD_MOVIE')
@@ -43,8 +52,9 @@ const movieReducer = ( state = {}, action ) => {
 			}
 		case 'GET_POPULAR_MOVIE':
 		console.log('action in GET_POPULAR_MOVIE')
+		console.log('action.popularMovieList -', action.popularMovieList)	
 			return {
-				movie: 456,
+				...state,
 				popularMovieList: action.popularMovieList
 			}	
 		default:
@@ -52,7 +62,8 @@ const movieReducer = ( state = {}, action ) => {
 	}
 }
 
-const rootReducer = combineReducers({ movie: movieReducer })
+// const rootReducer = combineReducers({ movie: movieReducer })
+const rootReducer = movieReducer
 
 // create middleware with redux-saga
 const sagaMiddleware = createSagaMiddleware()
@@ -68,9 +79,7 @@ const store = createStore(
 sagaMiddleware.run( rootSaga )
 
 // mapStateToProps, mapDispatchToProps
-const mapStateToProps = state => ({
-	popularMovieList: state.popularMovieList
-})
+const mapStateToProps = state => ({ popularMovieList: state.popularMovieList })
 
 const mapDispatchToProps = dispatch => ({
 	getPopularMovie: () => dispatch( getPopularMovie() )
