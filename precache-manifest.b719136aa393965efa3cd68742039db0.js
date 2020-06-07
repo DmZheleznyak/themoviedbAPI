f6b171d1d2ee62c6b679c5bb7d7fda3f2c9ab141
DmZheleznyak <11lione1991@gmail.com>
2020-06-07 11:28:18 +0300

    Updates

---
+++ precache-manifest.b719136aa393965efa3cd68742039db0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9050ef4cc8e5f19e89e9",
+    "url": "/themoviedbAPI/static/js/runtime~main.9050ef4c.js"
+  },
+  {
+    "revision": "25349b9d3d2d9028f77b",
+    "url": "/themoviedbAPI/static/js/main.25349b9d.chunk.js"
+  },
+  {
+    "revision": "cd2feff09699ff166f3f",
+    "url": "/themoviedbAPI/static/js/1.cd2feff0.chunk.js"
+  },
+  {
+    "revision": "25349b9d3d2d9028f77b",
+    "url": "/themoviedbAPI/static/css/main.f4df65aa.chunk.css"
+  },
+  {
+    "revision": "b622e11073929e6729960b92360e68e6",
+    "url": "/themoviedbAPI/index.html"
+  }
+];
